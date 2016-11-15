e7059ad3f846344f44d285e45cabb7ac5928bb72
Thomas Ricci <thomas.ricci@cpnv.ch>
2016-11-15 13:46:29 +0100

    Initial commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateImageRequest;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Http\Request;
+use App\Image;
+use Illuminate\Support\Facades\Storage;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("image.index");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return ('image.upload');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateImageRequest $request)
+    {
+        $image = Image::create($request->all());
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Image $image)
+    {
+        return Storage::disk('s3')->url($image->path);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Image $image)
+    {
+        return view('image.edit',$image);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return abort(403,"you should have never came here");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Image $image)
+    {
+        Storage::disk("s3")->delete($image->path);
+        $image->delete();
+        return redirect()->to("/images");
+    }
+}
