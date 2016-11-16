c16bcc488aa43be374aae7ee63c6565db294d975
thomas ricci <grisou13@hotmail.fr>
2016-11-16 19:48:29 +0100

    Added default album on user creation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,10 +62,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+        Album::create([
+          "name"=>"default",
+          "description"=>"",
+          "user"=>$user
+        ])
+        return $user;
     }
 }
