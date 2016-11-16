1ecff99ec205cb4c274295dd2ad4c29faf6aa1a6
Thomas Ricci <thomas.ricci@cpnv.ch>
2016-11-16 15:20:22 +0100

    Added HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware("auth",["except"=>["welcome"]]);
+    }
+    public function welcome(Request $request)
+    {
+        return view("welcome");
+    }
+    public function home(Request $request)
+    {
+        return view("home",["user"=>Auth::user()]);
+    }
 }
