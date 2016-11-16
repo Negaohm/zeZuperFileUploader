ccf4b98718a4d77f9acd3c547646894f35850211
Thomas Ricci <thomas.ricci@cpnv.ch>
2016-11-16 15:20:52 +0100

    Made UploadToCloud qeueable

--- app/Listeners/MoveToCloud.php
+++ app/Listeners/MoveToCloud.php
@@ -7,7 +7,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Storage;
 
-class MoveToCloud
+class MoveToCloud implements ShouldQueue
 {
     /**
      * Create the event listener.
