4aa30651e6d3a8f2b58f38d0a8f2dc0110d46f43
thomas ricci <grisou13@hotmail.fr>
2016-11-18 00:21:38 +0100

    Resolved conflict

--- app/Image.php
+++ app/Image.php
@@ -39,16 +39,13 @@ class Image extends Model
     }
     public function getThumbnailUrlAttribute()
     {
-        return array_key_exists("thumbnail_url",$this->attributes) ?: route("image.thumbnail",$this);
     }
     public function getPathAttribute()
     {
-        return storage_path("app/".$this->attributes["filename"]); //with the bcrypt signature we have, it should always be unique, and unreversible
     }
     public function setFilenameAttribute($value)
     {
         //make a hash out of the filename, album id, original filename and date
-        $this->attributes["original_filename"] = $value;
         $this->attributes["filename"] = Str::slug(Hash::make($value.$value.Carbon::now()->toTimeString()));
     }
 
